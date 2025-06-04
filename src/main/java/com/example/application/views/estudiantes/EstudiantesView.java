80c4fb57503e08f0c1c619996d26e731c978332e
start.vaadin.com <no-reply@vaadin.com>
2025-06-04 15:33:49 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/estudiantes/EstudiantesView.java
@@ -0,0 +1,57 @@
+package com.example.application.views.estudiantes;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Estudiantes")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.PENCIL_RULER_SOLID)
+@Uses(Icon.class)
+public class EstudiantesView extends Composite<VerticalLayout> {
+
+    public EstudiantesView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Grid stripedGrid = new Grid(SamplePerson.class);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow);
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Nuevo Estudiante");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        stripedGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        stripedGrid.setWidth("100%");
+        stripedGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(stripedGrid);
+        getContent().add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        getContent().add(stripedGrid);
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(VaadinSpringDataHelpers.toSpringPageRequest(query)).stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
